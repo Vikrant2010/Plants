c1ba316c013b46b9a2d2da508645aa40cd4064d5
Vikrant2010 <111408840+Vikrant2010@users.noreply.github.com>
2022-08-17 11:42:22 +0530

    Create auto-bot.ino

---
+++ auto-bot.ino
@@ -0,0 +1,28 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+ 
+void loop() {
+
+
+    int msvalue = analogRead(A0);
+    Serial.println(msvalue);
+ 
+ 
+ 
+     if (msvalue >= 500  )
+      {
+         digitalWrite(5, LOW);
+         digitalWrite(6, HIGH);
+        delay(1000);
+      }
+ 
+      else
+      {
+        digitalWrite(5, HIGH);
+        digitalWrite(6, LOW);
+        delay(1000);
+      }
